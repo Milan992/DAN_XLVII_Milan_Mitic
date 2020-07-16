ed02d3bd37cde400b21090b56ff0618fc16b5a21
Milan992 <mitic.gsm@gmail.com>
2020-07-16 16:06:29 +0200

    final commit

--- DAN_XLVII_Milan_Mitic/ConsoleAppVehicle/Program.cs
+++ DAN_XLVII_Milan_Mitic/ConsoleAppVehicle/Program.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAppVehicle
 {
     class Program
     {
+        static Random random = new Random();
+        static Vehicle vehicle = new Vehicle();
+        static Vehicle[] vehicles;
+
         static void Main(string[] args)
         {
+            int numberOfVehicles = random.Next(1, 16);
+            vehicles = new Vehicle[numberOfVehicles];
+            
+            for (int i = 0; i < vehicles.Length - 1; i++)
+            {
+                Thread.Sleep(4);
+                int j = random.Next(0, 2);
 
+                if (j == 1)
+                {
+                    vehicle.Direction = "North";
+                }
+                else
+                {
+                    vehicle.Direction = "South";
+
+                }
+                vehicles[i] = vehicle;
+                vehicles[i].VehicleCreated += VehiclesCreated;
+
+                Thread t = new Thread(() => vehicle.CrossBridge(vehicle));
+                t.Name = "Vehicle_" + i;
+                t.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void VehiclesCreated(object source, EventArgs e)
+        {
+                Console.WriteLine("{0} started in {1} direction", Thread.CurrentThread.Name, vehicle.Direction);
         }
     }
 }
---
+++ DAN_XLVII_Milan_Mitic/ConsoleAppVehicle/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVehicle
+{
+    class Vehicle
+    {
+        public string Direction { get; set; }
+        public bool Crossing = false;
+        public List<Vehicle> vehicles = new List<Vehicle>();
+
+        public delegate void VehicleCreatedEventHandler(object source, EventArgs args);
+
+        public event VehicleCreatedEventHandler VehicleCreated;
+
+        protected virtual void OnVehicleCreated()
+        {
+            VehicleCreated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void CrossBridge(Vehicle vehicle)
+        {
+            ManualResetEvent are = new ManualResetEvent(false);
+
+            Console.WriteLine("{0} started in {1} direction", Thread.CurrentThread.Name, vehicle.Direction);
+            while (vehicle.Direction != Direction && Crossing == true)
+            {
+                are.WaitOne();
+            }
+            Console.WriteLine("{0} waiting", Thread.CurrentThread.Name);
+            Crossing = true;
+            Direction = vehicle.Direction;
+            Console.WriteLine("{0} crossing the bridge", Thread.CurrentThread.Name);
+            Thread.Sleep(500);
+            Crossing = false;
+            Console.WriteLine("{0} crossed the bridge", Thread.CurrentThread.Name);
+        }
+    }
+}
